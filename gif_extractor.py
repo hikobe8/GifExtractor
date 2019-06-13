33cdf189de8501767d61cc30feebd230f99d9ad0
hikobe8 <hikobe8@163.com>
2019-06-13 18:31:49 +0800

    completion

---
+++ gif_extractor.py
@@ -0,0 +1,18 @@
+from PIL import Image
+import os
+import sys
+
+gifFileName = sys.argv[1]
+try:
+    im = Image.open(gifFileName)
+    pngDir = gifFileName[:-4]
+    os.mkdir(pngDir)
+    try:
+        while True:
+            current = im.tell()
+            im.save(os.path.join(pngDir, str(current) + '.png'))
+            im.seek(current + 1)
+    except EOFError:
+        pass
+except IOError:
+    print(gifFileName, ' not found')
